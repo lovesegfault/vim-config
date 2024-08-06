4e7789c232d60050edce4c98ed1ebd94b6c41d51
Bernardo Meurer <bernardo@meurer.org>
2024-08-06 13:33:26 -0400

    feat: enable tmux-navigator

--- config/default.nix
+++ config/default.nix
@@ -25,6 +25,7 @@
       enable = true;
       diagnostics = lib.mkIf config.plugins.lsp.enable "nvim_lsp";
     };
+    tmux-navigator.enable = true;
     treesitter = {
       enable = true;
       nixGrammars = true;
